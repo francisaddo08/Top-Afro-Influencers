3f3b052e5538417da03c4efefc5f232fa4015834
Francis Addo <francis.t.addo@gmail.com>
2024-12-01 10:00:04 +0000

    Updated Stored Procedure context

--- Afro.Ranking/Afro.Ranking.Persistance/ADO.NET/Abstract/ContextProcedure.cs
+++ Afro.Ranking/Afro.Ranking.Persistance/ADO.NET/Abstract/ContextProcedure.cs
@@ -10,7 +10,7 @@ namespace Afro.Ranking.Persistance.ADO.NET.Abstract
 {
     internal class ContextProcedure : IContextProcedure
     {
-       private readonly SqlCommand  Command;
+       private  SqlCommand  Command;
        public ContextProcedure( SqlCommand sqlCommand, string procedureName, int timeOut) 
        {
             Command = sqlCommand;
@@ -25,14 +25,68 @@ namespace Afro.Ranking.Persistance.ADO.NET.Abstract
         {
           Command.Parameters.AddWithValue(key, value);
         }
-        public ValueTask DisposeAsync() => throw new NotImplementedException();
-        public int ExecuteNonQuery() => throw new NotImplementedException();
-        public Task<int> ExecuteNonQueryAsync() => throw new NotImplementedException();
-        public object? ExecuteScalarData() => throw new NotImplementedException();
+
+        public int ExecuteNonQuery()
+        {
+            try
+            {
+              return  Command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw CreateException(ex);
+            }
+        }
+        public Task<int> ExecuteNonQueryAsync()
+        {
+            try
+            {
+                return Command.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                throw CreateException(ex);
+            }
+        }
+       
+        
+        public object ExecuteScalarData() 
+        {
+            try
+            {
+                return Command.ExecuteScalar();
+            }
+            catch (Exception ex)
+            { 
+            throw CreateException(ex);
+            }
+        }
         public Task<object?> ExecuteScalarDataAsync() => throw new NotImplementedException();
         public SqlDataReader ExecuteSqlDataReader() => throw new NotImplementedException();
         public Task<SqlDataReader> ExecuteSqlDataReaderAsync() => throw new NotImplementedException();
         public XmlReader ExecuteXmlReader() => throw new NotImplementedException();
         public Task<XmlReader> ExecuteXmlReaderAsync() => throw new NotImplementedException();
+
+        private Exception CreateException(Exception exception)
+        {
+            Exception ex = new Exception("Failed to execute stored procedure", exception);
+            ex.Data.Add("CommandText", Command.CommandText);
+            ex.Data.Add("parameters", GetParametersForCommand());
+
+            return ex;
+
+        }
+
+        private string GetParametersForCommand() 
+        {
+          StringBuilder sb = new StringBuilder();
+          foreach( SqlParameter param in Command.Parameters)
+          {
+                sb.Append($"{param.ParameterName}={param.Value}|");
+          }
+          return sb.ToString();
+        }
+        public ValueTask DisposeAsync() => Command.DisposeAsync();
+      
     }
 }
